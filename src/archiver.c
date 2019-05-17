2830efccf2d7c2081bfe0c7b2073cac52b9eb9e5
S. Guliaev <dev.sagul@gmail.com>
2019-05-17 05:34:02 +0300

    refactor main algo

--- src/archiver.c
+++ src/archiver.c
@@ -3,62 +3,44 @@
 #include <string.h>
 
 
-void print_usage(char *name)
+int print_usage(char *name)
 {
 	printf("Usage: %s [operation] archive [member...]", name);
 	printf("mods:\nt -- prints contents table of the given arhive.\n");
 	printf("c -- creates archive from given members.\n");
 	printf("x -- extracts contents of the archive into current directory.\n");
+	return EXIT_SUCCESS;
+}
+
+void exit_alt(char *error1, char *error2, int condition)
+{
+	fprintf(stderr, "%s\n", condition ? error1 : error2);
+	exit(EXIT_FAILURE);
 }
 
 int main (int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		print_usage(argv[0]);
-		return EXIT_SUCCESS;
-	}
+	char *mod;
 
-	option = argv[1]
-	if (!strcmp(option, "t"))
+	if (argc == 1)
+		return print_usage(argv[0]);
+	mod = argv[1];
+	if (!strcmp(mod, "t") || !strcmp(mod, "x"))
 	{
 		if (argc != 3)
-		{
-			error_msg = argc == 2 ?
-				"No archive provided\n" :
-				"To many arguments";
-			fprintf(2, "%s\n", error_msg);
-			return EXIT_FAILURE;
-		}
-		print_table(argv[2]);
-		return EXIT_SUCCESS;
+			exit_alt("No archive provided",
+				 "Too many arguments",
+				 argc == 2);
+		return !strcmp(mod, "t") ? print_table(argv[2]) : extract_archive(argv[2]);
 	}
-	if (!strcmp(option. "c"))
+	if (!strcmp(mod, "c"))
 	{
 		if (argc < 4)
-		{
-			error_msg = argc == 2 ?
-				"No archive name provided" :
-				"No arhive member(s) provided)";
-			fprintf(2, "%s\n", error_msg);
-			return EXIT_FAILURE;
-		}
-		create_archive(argv[2], argv + 3);
-		return EXIT_SUCCESS;
+			exit_alt("No archive name provided",
+				 "No archive member(s) provided)",
+				 argc == 2);
+		return create_archive(argv[2], argv + 3);
 	}
-	if (!strcmp(option, "x"))
-	{
-		if (argc != 3)
-		{
-			error_msg = argc == 2 ?
-				"No archive provided\n" :
-				"To many arguments";
-			fprintf(2, "%s\n", error_msg);
-			return EXIT_FAILURE;
-		}
-		extract_archive(argv[2]);
-		return EXIT_SUCCESS;
-	}
-	fprintf(2, "Unknown operation\n");
+	fprintf(stderr, "Unknown operation\n");
 	return EXIT_FAILURE;
 }
