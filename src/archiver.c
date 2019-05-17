075928995f871f17223d147a8479559aae45d617
S. Guliaev <dev.sagul@gmail.com>
2019-05-17 06:01:28 +0300

    fix style violation in the archiver.c file

--- src/archiver.c
+++ src/archiver.c
@@ -18,28 +18,26 @@ void exit_alt(char *error1, char *error2, int condition)
 	exit(EXIT_FAILURE);
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	char *mod;
 
 	if (argc == 1)
 		return print_usage(argv[0]);
 	mod = argv[1];
-	if (!strcmp(mod, "t") || !strcmp(mod, "x"))
-	{
+	if (!strcmp(mod, "t") || !strcmp(mod, "x")) {
 		if (argc != 3)
 			exit_alt("No archive provided",
 				 "Too many arguments",
 				 argc == 2);
 		return !strcmp(mod, "t") ? print_table(argv[2]) : extract_archive(argv[2]);
 	}
-	if (!strcmp(mod, "c"))
-	{
+	if (!strcmp(mod, "c")) {
 		if (argc < 4)
 			exit_alt("No archive name provided",
 				 "No archive member(s) provided)",
 				 argc == 2);
-		return create_archive(argv[2], argv + 3);
+		return create_archive(argv[2], argv + 3, argc - 3);
 	}
 	fprintf(stderr, "Unknown operation\n");
 	return EXIT_FAILURE;
